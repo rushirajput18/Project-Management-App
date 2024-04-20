524b179729daa98fddcb99f9869176d5b8893704
maheshthakare2003 <maheshthakare149@gmail.com>
2024-04-20 16:08:38 +0530

    further additions

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,19 @@
+const express=require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.env' });
+
+//code to connect to mongoose database
+const app=express();
+console.log(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{console.log('DB connection established')});
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
