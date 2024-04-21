44b99527cf1ff64f230b2735d0767da8b215964a
OmkumarD <omkumardhanorkar098@gmail.com>
2024-04-21 09:43:31 +0530

    new task add

--- frontend/src/views/admin/marketplace/index.jsx
+++ frontend/src/views/admin/marketplace/index.jsx
@@ -10,30 +10,14 @@ import {
   useColorModeValue,
   SimpleGrid,
 } from "@chakra-ui/react";
-
-// Custom components
-// import Banner from "views/admin/marketplace/components/Banner";
-// import ProjectDetails from "views/admin/marketplace/components/ProjectDetails";
-import NewProject from "views/admin/marketplace/components/TableTopCreators";
-// import HistoryItem from "views/admin/marketplace/components/HistoryItem";
 import NFT from "components/card/NFT";
-import Card from "components/card/Card.js";
 import AddNFTCard from "./AddNFTCard"; // Import the new component
-import { AddIcon } from '@chakra-ui/icons';
 
-// Assets
-// import Nft1 from "assets/img/nfts/Nft1.png";
-// import Nft2 from "assets/img/nfts/Nft2.png";
-// import Nft3 from "assets/img/nfts/Nft3.png";
 import Nft4 from "assets/img/nfts/Nft4.png";
-// import Nft5 from "assets/img/nfts/Nft5.png";
-// import Nft6 from "assets/img/nfts/Nft6.png";
 import Avatar1 from "assets/img/avatars/avatar1.png";
 import Avatar2 from "assets/img/avatars/avatar2.png";
 import Avatar3 from "assets/img/avatars/avatar3.png";
 import Avatar4 from "assets/img/avatars/avatar4.png";
-import tableDataTopCreators from "views/admin/marketplace/variables/tableDataTopCreators.json";
-import { tableColumnsTopCreators } from "views/admin/marketplace/variables/tableColumnsTopCreators";
 
 export default function Marketplace() {
   const [nftCards, setNFTCards] = useState([
@@ -114,16 +98,6 @@ export default function Marketplace() {
 </SimpleGrid>
           </Flex>
         </Flex>
-        {/* <Flex
-          flexDirection='column'
-          gridArea={{ xl: "1 / 3 / 2 / 4", "2xl": "1 / 2 / 2 / 3" }}>
-  <Card px='20px' py='8px' mb='30px' mt='6px'>
-            <NewProject
-              tableData={tableDataTopCreators}
-              columnsData={tableColumnsTopCreators}
-            />
-          </Card>
-        </Flex> */}
       </Grid>
       {/* Delete Product */}
     </Box>
