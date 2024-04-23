83fa2e6e599d869169ea107acfe0545690e364b1
maheshthakare2003 <maheshthakare149@gmail.com>
2024-04-23 09:56:14 +0530

    employee rating

--- frontend/src/views/admin/marketplace/index.jsx
+++ frontend/src/views/admin/marketplace/index.jsx
@@ -1,30 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,useEffect } from "react";
 import axios from "axios";
-import { Box, Flex, Grid, Text, useColorModeValue, SimpleGrid } from "@chakra-ui/react";
-import AddNFTCard from "./AddNFTCard";
-
-// New ProjectCard component
-const ProjectCard = ({ project }) => {
-  const { title, description, leader, employees } = project;
-  return (
-    <Box bg="gray.200" p={4} borderRadius="md">
-      <Text fontWeight="bold" mb={2}>
-        {title}
-      </Text>
-      <Text mb={2}>{description}</Text>
-      <Text>Leader: {leader ? leader.name : "Unknown"}</Text>
-      <Text>
-        Employees: {employees.map((employee) => employee.name).join(", ")}
-      </Text>
-    </Box>
-  );
-};
-
+// Chakra imports
+import {
+  
+  Box,
+  Flex,
+  Grid,
+  Text,
+  useColorModeValue,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import NFT from "components/card/NFT";
+import NFT1 from "assets/img/nfts/Nft1.png"
+import AddNFTCard from "./AddNFTCard"; // Import the new componen
 export default function Marketplace() {
-  const [projects, setProjects] = useState([]);
-  const textColor = useColorModeValue("secondaryGray.900", "white");
+  const [nftCards, setNFTCards] = useState([]);
 
   useEffect(() => {
+    // Fetch project data from the backend API
     const fetchProjects = async () => {
       try {
         const response = await axios.get("http://localhost:5000/project/allProjects");
@@ -42,35 +35,36 @@ export default function Marketplace() {
         setNFTCards(mappedNFTCards);
       } catch (error) {
         console.error("Error fetching projects:", error);
-      }
+      } 
     };
+
     fetchProjects();
-  }, []);
+  }, []); // Empty dependency array to run the effect only once
 
-  useEffect(() => {
-    const fetchUpdatedProjects = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/project/allProjects");
-        const updatedProjectData = response.data.data.projects;
-        setProjects(updatedProjectData);
-      } catch (error) {
-        console.error("Error fetching updated projects:", error);
-      }
-    };
-    fetchUpdatedProjects();
-  }, [projects]);
-
+  // Chakra Color Mode
+  const textColor = useColorModeValue("secondaryGray.900", "white");
+  // const textColorBrand = useColorModeValue("brand.500", "white");
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
+      
+      {/* Main Fields */}
       <Grid
-        mb="20px"
+        mb='20px'
         gridTemplateColumns={{ xl: "repeat(3, 1fr)", "2xl": "1fr 0.46fr" }}
         gap={{ base: "20px", xl: "20px" }}
-        display={{ base: "block", xl: "grid" }}
-      >
-        <Flex flexDirection="column" gridArea={{ xl: "1 / 1 / 2 / 4", "2xl": "1 / 1 / 2 / 4" }}>
-          <Flex direction="column">
-            <Text mt="25px" mb="36px" color={textColor} fontSize="2xl" ms="24px" fontWeight="700">
+        display={{ base: "block", xl: "grid" }}>
+        <Flex
+          flexDirection='column'
+          gridArea={{ xl: "1 / 1 / 2 / 4", "2xl": "1 / 1 / 2 / 4" }}>
+          {/* <Banner /> */}
+          <Flex direction='column'>
+            <Text
+              mt='25px'
+              mb='36px'
+              color={textColor}
+              fontSize='2xl'
+              ms='24px'
+              fontWeight='700'>
               All Projects
             </Text>
             <SimpleGrid columns={{ base: 1, md: 4 }} gap="20px" mb={{ base: "20px", xl: "0px" }}>
@@ -90,6 +84,7 @@ export default function Marketplace() {
           </Flex>
         </Flex>
       </Grid>
+      {/* Delete Product */}
     </Box>
   );
 }
