a3a3c60d8f9b38ed5383b165fb564cf2a4708c2b
rushirajput18 <rushikeshrajput666@gmail.com>
2024-07-14 18:46:50 +0530

    Logo and Navbar

--- frontend/src/components/sidebar/components/Brand.js
+++ frontend/src/components/sidebar/components/Brand.js
@@ -1,19 +1,19 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, useColorModeValue, Image } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
+import logo from 'components/icons/image.png';
 import { HSeparator } from "components/separator/Separator";
 
 export function SidebarBrand() {
-  //   Chakra color mode
+  // Chakra color mode
   let logoColor = useColorModeValue("navy.700", "white");
 
   return (
     <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+      <Image src={logo} h='66px' w='400px' my='32px' alt='Logo' color={logoColor} />
       <HSeparator mb='20px' />
     </Flex>
   );
